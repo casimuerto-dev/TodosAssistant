a9d52d1359e87420a38067db34f7af2b57a5ef57
DariOrtega <casi@elemento43.com>
2022-04-25 19:59:03 -0500

    fix: textarea cursor on left. fix: searchBar remove placeholder on focus

--- src/TodoSearch/index.js
+++ src/TodoSearch/index.js
@@ -2,8 +2,20 @@ import React from "react";
 import "./TodoSearch.css";
 
 export const TodoSearch = ({ loading, searchValue, handleSearchChange }) => {
+  const handleFocus = () => {
+    document.querySelector(".searchBar").setAttribute("placeholder", "");
+  };
+  const handleBlur = () => {
+    document
+      .querySelector(".searchBar")
+      .setAttribute("placeholder", "¿Qué buscas?");
+    handleSearchChange({ target: { value: "" } });
+  };
+
   return (
     <input
+      onBlur={handleBlur}
+      onFocus={handleFocus}
       maxLength={"35"}
       value={searchValue}
       className="searchBar"
