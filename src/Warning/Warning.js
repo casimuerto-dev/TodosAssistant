267e52e47be7034e5b5ea3b30923fe17d61bd178
DariOrtega <casi@elemento43.com>
2022-04-27 13:03:12 -0500

    Improve: Warning re-render improvement

--- src/Warning/Warning.js
+++ src/Warning/Warning.js
@@ -2,14 +2,15 @@ import React from "react";
 import "./warningStyles.css";
 
 export const Warning = (props) => {
+  const handleEvent = (e) => {
+    if (e.key === "Escape") {
+      props.children[1].props.children[0].props.onClick();
+    } else if (e.key === "Enter") {
+      props.children[1].props.children[1].props.onClick();
+    }
+  };
+
   React.useEffect(() => {
-    const handleEvent = (e) => {
-      if (e.key === "Escape") {
-        props.children[1].props.children[0].props.onClick();
-      } else if (e.key === "Enter") {
-        props.children[1].props.children[1].props.onClick();
-      }
-    };
     window.addEventListener("keydown", handleEvent);
     return () => window.removeEventListener("keydown", handleEvent);
   });
